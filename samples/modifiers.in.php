c63e4cb493a2d64f082d8e8ccbab92a6a1fa755b
Bas Stottelaar <basstottelaar@gmail.com>
2015-12-15 09:06:00 +0100

    Fix for comparing types, but now for properties. Refs https://github.com/naneau/php-obfuscator/pull/27.

---
+++ samples/modifiers.in.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Test;
+
+class Foo
+{
+    public $publicProperty;
+
+    protected $protectedProperty;
+
+    private $privateProperty;
+
+    public function myPublicMethod()
+    {
+    }
+
+    protected function myProtectedMethod()
+    {
+    }
+
+    private function myPrivateMethod()
+    {
+    }
+
+    final private function myFinalPrivateMethod()
+    {
+    }
+}
---
+++ samples/modifiers.out.php
@@ -0,0 +1,21 @@
+<?php
+namespace Test;
+
+class Foo
+{
+    public $publicProperty;
+    protected $protectedProperty;
+    private $spe20445;
+    public function myPublicMethod()
+    {
+    }
+    protected function myProtectedMethod()
+    {
+    }
+    private function sp7d6c4f()
+    {
+    }
+    private final function spc35f08()
+    {
+    }
+}
